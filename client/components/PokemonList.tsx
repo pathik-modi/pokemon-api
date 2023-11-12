b9159b36cd34d5d2ff5371ed1b6821517d9a99f5
Pathik Modi <modipathik95@gmail.com>
2023-11-13 11:37:22 +1300

    add pokemon list for generation 1

--- client/components/PokemonList.tsx
+++ client/components/PokemonList.tsx
@@ -1,18 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
+import { fetchPokemonGeneration } from '../apis/pokemon'
+
 export default function PokemonList() {
-  const generation = {
-    name: 'generation-i',
-    region: 'Kanto',
-    pokemon: [{ id: 1, name: 'Bulbasaur' }],
+  const { isPending, error, data } = useQuery({
+    queryKey: ['generation'],
+    queryFn: () => fetchPokemonGeneration(1),
+  })
+
+  if (isPending) {
+    return 'Loading...'
+  }
+
+  if (error) {
+    return 'An error has occurred: ' + error.message
   }
 
   return (
     <>
-      <h2>Pokémon in {generation.region}:</h2>
-      <ul>
-        {generation.pokemon.map((p) => (
-          <li key={p.id}>{p.name}</li>
-        ))}
-      </ul>
+      <h2>Pokémon in {data.region}:</h2>
+      <ul>{data && data.pokemon.map((p) => <li key={p.id}>{p.name}</li>)}</ul>
     </>
   )
 }
